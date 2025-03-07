036a96d773a7e864f0f66fa6c226b3c057f46efd
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-07 14:44:30 +0600

    Create assci_value.c

---
+++ assci_value.c
@@ -0,0 +1,19 @@
+/// get Ascii value with c
+
+#include<stdio.h>
+int main(){
+
+char ch;
+
+printf("Enter a character (A-Z / a-z) : ");
+scanf("%c",&ch);
+
+printf("The Ascci Value of '%c' is %d\n ",ch,ch);
+
+return 0;
+}
+
+
+
+
+
