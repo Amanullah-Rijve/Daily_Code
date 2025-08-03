c50fbf4f4a288d7c930cb4bbae65ba2474e55740
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-03 18:06:55 +0600

    Create program.c

---
+++ program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+
+int location=3;
+
+int p1=36;
+int p2=75;
+int p3=43;
+
+printf("total location %d\n",location);
+int total =(p1+p2+p3);
+printf("Total pacages %d",total);
+
+
+return 0;
+}
