a266413b7615b8cc69e5213a17e84b5bdf8e7e11
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-08-03 18:07:29 +0600

    Create pro.c

---
+++ pro.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+/// declare 4 float variables
+
+printf("Enter 4 float values:");
+
+float A,B,C,D;
+scanf("%f %f %f %f",&A,&B,&C,&D);
+
+float total = (A+B+C+D);
+
+printf("Total Floodgains %f",total);
+
+
+return 0;
+}
